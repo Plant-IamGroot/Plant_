c4d14286ff19e59b5cdf96c2f9c8d1297ede24ed
CHOIBYOUNGGUK <qudrnr102403@gmail.com>
2023-12-14 00:27:33 +0900

    -- First Commit --

---
+++ IamGroot/src/main/java/Plant/IamGroot/IamGrootApplication.java
@@ -0,0 +1,13 @@
+package Plant.IamGroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamGrootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamGrootApplication.class, args);
+	}
+
+}
---
+++ IamGroot/src/test/java/Plant/IamGroot/IamGrootApplicationTests.java
@@ -0,0 +1,13 @@
+package Plant.IamGroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamGrootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
